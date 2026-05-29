c048211d650183a28496e074ba34ac9e44f0882c
Michsim253 <mich.simonova@gmail.com>
2026-05-29 13:34:46 +0200

    Update dino.py

--- dino.py
+++ dino.py
@@ -1,1 +1,10 @@
-"AQ.Ab8RN6L3-8KocWOaO9faph_r-hJbPId7xWX3VTZyjuYyF6AQzA"
+import streamlit as st
+
+st.set_page_config(page_title="DinoPátrač", page_icon="🦕")
+
+st.title("🦕 DinoPátrač")
+st.write("Ak vidíš tento text, tvoj Streamlit a GitHub sú prepojené SPRÁVNE! 🎉")
+
+if st.button("Spustiť balóniky"):
+    st.balloons()
+    st.success("Funguje to!")
